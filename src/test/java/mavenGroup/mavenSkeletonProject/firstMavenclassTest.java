fe82bd7b0656b8723dee2d4db661ac412019af3c
TejaswiniBoyapatiRepo <boyapatitejaswini@gmail.com>
2024-04-16 09:18:56 +0530

    pusing maven project to github

---
+++ src/test/java/mavenGroup/mavenSkeletonProject/firstMavenclassTest.java
@@ -0,0 +1,18 @@
+package mavenGroup.mavenSkeletonProject;
+
+import org.testng.annotations.Test;
+
+public class firstMavenclassTest {
+
+	@Test
+	public void creation()
+	{
+		System.out.println("Creating a maven class");
+	}
+	
+	@Test
+	public void editingTest()
+	{
+		System.out.println("editing a maven class file");
+	}
+}
---
+++ src/test/java/mavenGroup/mavenSkeletonProject/secondMavenclassTest.java
@@ -0,0 +1,19 @@
+package mavenGroup.mavenSkeletonProject;
+
+import org.testng.annotations.Test;
+
+public class secondMavenclassTest {
+	
+	@Test
+	public void login()
+	{
+		System.out.println("logging into the browser");
+	}
+	
+	@Test
+	public void closing()
+	{
+		System.out.println("closing the browser");
+	}
+
+}
